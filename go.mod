92aa2d87e198d2cf83bafc4c12f339e3c6eb4308
platon-p <platonpechenev@ya.ru>
2022-05-08 17:24:59 +0300

    CRUD

--- go.mod
+++ go.mod
@@ -12,11 +12,16 @@ require (
 	github.com/GeertJohan/go.rice v1.0.0 // indirect
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/daaku/go.zipexe v1.0.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/compress v1.15.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.12 // indirect
 	github.com/phpdave11/gofpdi v1.0.8 // indirect
 	github.com/pkg/errors v0.8.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.34.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
+	gorm.io/driver/sqlite v1.3.2 // indirect
+	gorm.io/gorm v1.23.5 // indirect
 )
--- main.go
+++ main.go
@@ -1,61 +1,48 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/72nd/gopdf-wrapper"
 	"github.com/72nd/gopdf-wrapper/fonts"
 	"github.com/gofiber/fiber/v2"
 	"github.com/signintech/gopdf"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"log"
 	"math/rand"
+	"strconv"
 	"strings"
 )
 
-const letters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890"
-const background_path = "./backgrounds"
+const (
+	letters        = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890"
+	backgroundPath = "./backgrounds"
+)
 
 type Preset struct {
-	name         string
-	background   string
-	greetingText string
-	greetingY    int
-	text         string
-	textX        int
-	textY        int
+	gorm.Model
+	Name      string  `json:"Name"`
+	PaperSize string  `json:"PaperSize"`
+	Text      string  `json:"Text"`
+	Greeting  string  `json:"Greeting"`
+	TextX     float64 `json:"TextX"`
+	TextY     float64 `json:"TextY"`
+	GreetingY float64 `json:"GreetingY"`
+	Image     string  `json:"Image"`
 }
 
 var gramota = Preset{
-	name:         "Благодарственное письмо",
-	background:   background_path + "/gramota.jpg",
-	greetingText: "Здравствуйте, {}!",
-	greetingY:    50,
-	text:         "Laboris ad esse ullamco consectetur eiusmod do nisi nisi sunt dolor in incididunt et veniam do. Laboris ad esse ullamco consectetur eiusmod do nisi nisi sunt dolor in incididunt et veniam do.",
-	textX:        40,
-	textY:        70,
+	Name:      "Благодарственное письмо",
+	PaperSize: "A4",
+	Text:      "Ea tempor in pariatur ea enim nulla eiusmod. Nulla fugiat consequat occaecat est id consectetur Lorem voluptate ut amet sunt tempor. Nulla fugiat consequat occaecat est id consectetur Lorem voluptate ut amet sunt tempor.",
+	Greeting:  "[Уважаемый][Уважаемая] {}!",
+	TextX:     30,
+	TextY:     60,
+	GreetingY: 30,
+	Image:     backgroundPath + "/gramota.jpg",
 }
-
-func renderPdf(name, greeting string) string {
-	doc, err := gopdfwrapper.NewDoc(18, 1)
-	if err != nil {
-		fmt.Print(err)
-	}
-
-	liberation, _ := fonts.NewLiberationSansFamily()
-	doc.AddPage()
-
-	doc.SetFontFamily(*liberation)
-
-	doc.Image("gramota.jpg", 0, 0, gopdf.PageSizeA4)
-
-	doc.SetY(50)
-
-	greeting = strings.Replace(greeting, "{}", "%v", 1)
-	doc.SetX(0)
-	doc.CellWithOption(gopdf.PageSizeA4, fmt.Sprintf(greeting, name), gopdf.CellOption{Align: gopdf.Center})
-
-	pathName := "results/" + generateName() + ".pdf"
-	doc.WritePdf(pathName)
-	return pathName
-}
+var db *gorm.DB
 
 func generateName() string {
 	s := ""
@@ -79,57 +66,115 @@ func proPdf(name string, cType Preset) string {
 	}
 
 	liberation, _ := fonts.NewLiberationSansFamily()
-	doc.SetFontFamily(*liberation)
+	_ = doc.SetFontFamily(*liberation)
 	doc.AddPage()
 
-	doc.Image(cType.background, 0, 0, gopdf.PageSizeA4)
+	_ = doc.Image(cType.Image, 0, 0, gopdf.PageSizeA4)
 
-	doc.SetY(float64(cType.greetingY))
-	doc.SetFontStyle("bold")
-	greeting := strings.Replace(cType.greetingText, "{}", "%v", 1)
-	doc.CellWithOption(gopdf.PageSizeA4, fmt.Sprintf(greeting, name), gopdf.CellOption{Align: gopdf.Center})
+	doc.SetY(cType.GreetingY)
+	_ = doc.SetFontStyle("bold")
+	greeting := strings.Replace(cType.Greeting, "{}", "%v", 1)
+	_ = doc.CellWithOption(gopdf.PageSizeA4, fmt.Sprintf(greeting, name), gopdf.CellOption{Align: gopdf.Center})
 
-	doc.SetFontStyle("")
+	_ = doc.SetFontStyle("")
 
-	//w := 420 - cType.textX
-	//symbW, _ := doc.MeasureTextWidth("Ж")
-	//onOneLineSymbs := int(float64(w) / symbW)
 	lineN := 0.
 	lastSt := ""
 
-	for _, st := range strings.Split(cType.text, " ") {
+	for _, st := range strings.Split(cType.Text, " ") {
 		l, _ := doc.MeasureTextWidth(lastSt + st + " ")
-		if l <= float64(210-cType.textX*2+1) {
+		if l <= 210-cType.TextX*2+1 {
 			lastSt += st + " "
 		} else {
-			doc.SetX(float64(cType.textX))
-			doc.SetY(float64(cType.textY) + lineN*(doc.LineHeight(18)+1))
-			doc.Text(lastSt)
+			doc.SetX(cType.TextX)
+			doc.SetY(cType.TextY + lineN*(doc.LineHeight(18)+1))
+			_ = doc.Text(lastSt)
 
 			lineN++
 			lastSt = st + " "
 		}
-		//
-		//if len(lastSt+st+" ") <= onOneLineSymbs+1 {
-		//	lastSt += st + " "
-		//} else {
-		//	doc.AddText(float64(cType.textX), float64(cType.textY)+lineN*doc.LineHeight(18), lastSt)
-		//	lineN++
-		//	lastSt = st + " "
-		//}
 	}
-	doc.SetX(float64(cType.textX))
-	doc.SetY(float64(cType.textY) + lineN*(doc.LineHeight(18)+1))
-	doc.Text(lastSt)
-	//doc.AddWrapText(float64(cType.textX), float64(cType.textY), 210, cType.text)
+	doc.SetX(cType.TextX)
+	doc.SetY(cType.TextY + lineN*(doc.LineHeight(18)+1))
+	_ = doc.Text(lastSt)
 
 	pathName := "results/" + generateName() + ".pdf"
-	doc.WritePdf(pathName)
+	_ = doc.WritePdf(pathName)
 	return pathName
 }
 
+func initDatabase() {
+	var err error
+	db, err = gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
+	if err != nil {
+		fmt.Println("Connection error")
+	}
+	db.AutoMigrate()
+}
+
+func newPreset(c *fiber.Ctx) error {
+	var preset Preset
+	err := json.Unmarshal(c.Body(), &preset)
+	if err != nil {
+		return fiber.NewError(400, "Can not parse json")
+	}
+	db.Create(&preset)
+	return c.Redirect("/api/presets")
+}
+
+func getPreset(c *fiber.Ctx) error {
+	id := c.Query("id")
+	if id == "" {
+		return fiber.NewError(400, "Id field is required")
+	}
+	var preset Preset
+	idInt, err := strconv.Atoi(id)
+	if err != nil {
+		return fiber.NewError(400, "Could not parse id")
+	}
+	db.First(&preset, idInt)
+	presetJson, _ := json.Marshal(preset)
+	if err != nil {
+		return fiber.NewError(500, "Could not create JSON")
+	}
+	return c.Send(presetJson)
+}
+
+func updatePreset(c *fiber.Ctx) error {
+	id := c.Query("id")
+	param := strings.Split(strings.Split(c.OriginalURL(), "&")[1], "=")
+
+	db.Model(&Preset{}).Where("ID = "+id).Update(param[0], c.Query(param[0]))
+	return c.Redirect("preset?id=" + id)
+}
+
+func deletePreset(c *fiber.Ctx) error {
+	id := c.Query("id")
+	if id == "" {
+		return fiber.NewError(400, "Id field is required")
+	}
+	var preset Preset
+	idInt, err := strconv.Atoi(id)
+	if err != nil {
+		return fiber.NewError(400, "Could not parse id")
+	}
+	db.First(&preset, idInt)
+	db.Delete(&preset)
+	return fiber.NewError(200, "Success")
+}
+
+func getPresets(c *fiber.Ctx) error {
+	var presets []Preset
+	db.Find(&presets)
+	presetsBody, _ := json.Marshal(presets)
+	return c.Send(presetsBody)
+}
+
 func main() {
+	initDatabase()
+
 	app := fiber.New()
+	app.Static("/backgrounds", backgroundPath)
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Render("./templates/index.html", fiber.Map{})
@@ -137,6 +182,14 @@ func main() {
 
 	api := app.Group("/api")
 	api.Get("/pdf", sendPdf)
+	api.Post("/new_preset", newPreset)
+	api.Get("/preset", getPreset)
+	api.Get("/update_preset", updatePreset)
+	api.Get("/delete_preset", deletePreset)
+	api.Get("/presets", getPresets)
 
-	app.Listen(":8000")
+	err := app.Listen(":8000")
+	if err != nil {
+		log.Fatalln("Oops...")
+	}
 }
